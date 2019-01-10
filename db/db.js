a14d0166ac2aa7fa6e9044676ea00ba66791b57e
Andrii Kost <andriikost@Andriis-MacBook-Pro.local>
2019-01-10 08:20:52 -0600

    REST API

---
+++ db/db.js
@@ -0,0 +1,19 @@
+// import * as objects from './objects.json'
+
+// export { objects };
+const objects =  [
+    {
+        id: 1,
+        name: 'Downtown Cafe',
+        coordinates: [ -89.38472807407379, 43.07334730355585 ],
+        address: '22 S. Carroll St., Madison, WI 53703'
+    },
+    {
+        id: 2,
+        name: 'Downtown Bar',
+        coordinates: [ -89.38572807407379, 43.07434730355585 ],
+        address: '55 State St., Madison, WI 53701'
+    }
+];
+
+export default objects;
